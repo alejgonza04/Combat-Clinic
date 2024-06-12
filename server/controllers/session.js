64bd09ac134679072f11f02bd5a31f852f65438e
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-12 15:23:29 -0400

    Update session.js

--- server/controllers/session.js
+++ server/controllers/session.js
@@ -3,38 +3,26 @@ import mongoose from 'mongoose';
 
 import SessionMessage from '../models/sessionMessage.js';
 
-export const getSessions = async (req, res) => { 
-    try {
-        const sessionMessages = await SessionMessage.find();
-                
-        res.status(200).json(sessionMessages);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
-export const getSession = async (req, res) => { 
-    const { id } = req.params;
-
-    try {
-        const session = await SessionMessage.findById(id);
-        
-        res.status(200).json(session);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
 export const addSession = async (req, res) => {
     const { sessionType, sessionLength, sparringTime, techniques, date } = req.body;
+    const userEmail = req.user.email; 
 
-    const newSessionMessage = new SessionMessage({ sessionType, sessionLength, sparringTime, techniques, date })
+    const newSessionMessage = new SessionMessage({ sessionType, sessionLength, sparringTime, techniques, date, userEmail });
 
     try {
         await newSessionMessage.save();
-
         res.status(201).json(newSessionMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
+    }
+}
+
+export const getSessions = async (req, res) => { 
+    const userEmail = req.user.email; 
+    try {
+        const sessionMessages = await SessionMessage.find({ userEmail });
+        res.status(200).json(sessionMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
