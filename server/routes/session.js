c95555d86bb95ad254c94f6bd76bb37185f61e9d
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-12 15:24:22 -0400

    Update session.js

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getSessions, addSession } from '../controllers/session.js';
 import auth from '../middleware/auth.js';
+import passport from 'passport';
 
 const router = express.Router();
 
 router.get('/:userEmail', auth, getSessions);
-router.post('/', auth, addSession);
+router.post('/:userEmail', auth, addSession);
 
 export default router;
